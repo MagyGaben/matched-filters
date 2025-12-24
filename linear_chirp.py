66ecd9306b8ba90c7c1bd8edc0e8b4a3134d535b
MagyGaben <gabor.4441@gmail.com>
2025-12-24 22:56:08 +0100

    gaussian mf implemented

--- linear_chirp.py
+++ linear_chirp.py
@@ -1,24 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import chirp, convolve
+from scipy.signal import chirp, convolve, gausspulse
 
 # working with [us,MHz]
 res = 1e4
 T = 10
-t = np.arange(0, T + 1/res, 1/res)
-B = 2
-f_0 = 1
+t = np.arange(0, T, 1/res)
+B = 12
+f_0 = 40000
 phi = np.pi/1.7
 
 s_linchirp = chirp(t, f0=f_0, t1=T, f1=f_0 + B, method='linear')
-matched_filter = np.conj(s_linchirp[::-1])
+
+t_sym = np.concatenate((-t[len(t)//2-1::-1], t[:len(t)//2]))
+s_gauss = gausspulse(t_sym,f_0,B)
+print(t_sym)
+
+
+signal = s_gauss
+matched_filter = np.conj(signal[::-1])
 # matched_filter = np.conj(s_linchirp)
 
-SNR_dB = -13
-signal_power = np.mean(np.abs(s_linchirp)**2)
+SNR_dB = -30
+signal_power = np.mean(np.abs(signal)**2)
 noise_power = signal_power / (10**(SNR_dB/10))
-noise = np.sqrt(noise_power) * np.random.randn(len(s_linchirp))
-rx = (s_linchirp + noise) * np.exp(1j*phi)
+noise = np.sqrt(noise_power) * np.random.randn(len(signal))
+
+rx = (signal + noise) * np.exp(1j*phi)
 
 y = convolve(rx, matched_filter, mode='same')
 # y = np.correlate(rx, matched_filter, mode='same')
